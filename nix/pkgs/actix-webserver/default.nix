e7ba48aafe00b453fbad7d1a1e0026367ddb84d7
knoff <selby@niko.ink>
2024-05-04 23:07:54 +0200

    pre-rename

---
+++ nix/pkgs/actix-webserver/default.nix
@@ -0,0 +1,41 @@
+{ pkgs, rustPlatform }:
+rustPlatform.buildRustPackage rec {
+  pname = "app";
+  version = "0.1.0";
+  src = ./.;
+
+  nativeBuildInputs = with pkgs; [
+    (rust-bin.stable."1.76.0".default.override {
+      extensions = [ "rust-src" ];
+      targets = [ "wasm32-unknown-unknown" ];
+    })
+    pkg-config
+    trunk
+    binaryen
+    dart-sass
+    tailwindcss
+    wasm-bindgen-cli
+  ];
+
+  buildInputs = pkgs.commonBuildInputs ++ [
+    pkgs.super-tiny-icons
+  ];
+
+  cargoLock.lockFile = ./Cargo.lock;
+
+  buildPhase = ''
+    runHook preBuild
+    mkdir -p $TMPDIR/output
+    mkdir -p icons
+    cp -r ${pkgs.super-tiny-icons}/* icons/
+    trunk build --release --offline --dist $TMPDIR/output --public-url /
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/lib
+    cp -r $TMPDIR/output/* $out/lib/
+    runHook postInstall
+  '';
+}
