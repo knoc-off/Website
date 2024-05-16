1c6f65298cf28584184475ea1787507643295abe
knoff <selby@niko.ink>
2024-05-16 12:12:54 +0200

    Update about me sections
    In index.scss:
    - Changed the max-width of .about-container to 50rem
    - Changed the margin of .markdown-content to -10px
    
    In about_section.rs:
    - Updated the content of the about me section

--- nix/pkgs/portfolio/src/components/about_section.rs
+++ nix/pkgs/portfolio/src/components/about_section.rs
@@ -6,14 +6,12 @@ use super::social_links::*;
 #[function_component(AboutSection)]
 pub fn about_section() -> Html {
     let markdown_content = r#"
-I am a self-taught programmer. I have developed skills in Rust programming and security systems.
+I am a self-taught programmer with skills in Rust programming and security systems.
 
-I completed an IT course at the Technical University of Berlin, excelling in programming, security, system administration, network management, and web development.
+In 2023, I completed an IT course at the Technical University of Berlin, covering general topics within programming, security, system administration, network management, and web development.
 
-I have a focus on NixOS, enhancing my proficiency in Linux and system/server management.
-
-I am interested in exploring the potential applications of NixOS in CI/CD environments.
-    "#.to_string();
+Through my usage/focus of NixOS, I enhanced my Linux and system/server management skills and am most interested in exploring the potential applications of NixOS in CI/CD environments.
+"#.to_string();
 
 
     html! {
